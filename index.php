20d5b70ad789c1f61d43cca941af9713b52ce037
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-21 04:53:38 +0600

    max

--- index.php
+++ index.php
@@ -8,72 +8,56 @@
 
   include('page_sections/index_sections/section_one.php');
 
-  
+  include('page_sections/index_sections/section_two.php');
+
+  include('page_sections/index_sections/section_three.php');
+
+  include('page_sections/index_sections/section_four.php');
 ?>
 
+
 <section>
-<div class="card_div">
-<div class="container">
-  <div class="row">
+
+<div class="parallax">
+  <h5 class="text-center testi-head">Testimonials</h5>
+  <h3 class="text-center testi-head testi-head-sec">What Our Clients Say</h3>
+
+  <div class="container">
+  <div class="row testi-row">
     <div class="col-sm">
-      
-  
-  <div class="row no-gutters">
-    <div class="col-md-4">
-    <i class="fa fa-mobile fa-fw fa-4x" aria-hidden="true"></i>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
+    <div class="card testi-card" style="width: 18rem;">
+  <img class="card-img-top testi-img" src="./assets/images/adult-beard-boy-casual-220453.jpg" alt="">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
-
-</div>
+    </div>
     <div class="col-sm">
-   
-    <div class="row no-gutters">
-    <div class="col-md-4">
-    <i class="fa fa-envelope fa-fw fa-3x" aria-hidden="true"></i>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    
-    </div>
+    <div class="card testi-card" style="width: 18rem;">
+  <img class="card-img-top testi-img" src="./assets/images/women-s-white-and-black-button-up-collared-shirt-774909.jpg" alt="">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
-
-</div>
+    </div>
     <div class="col-sm">
-  
-    <div class="row no-gutters">
-    <div class="col-md-4">
-   <i class="fa fa-map-marker fa-fw fa-3x" aria-hidden="true"></i>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
- 
-    </div>
+    <div class="card testi-card" style="width: 18rem;">
+  <img class="card-img-top testi-img" src="./assets/images/man-crossed-arms-1516680.jpg" alt="">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
-
     </div>
   </div>
-</div>
-
-
-
 
 
 </div>
+
+</div>
+
+
 
 </section>
-
 </body>
 
 <?php
---
+++ page_sections/index_sections/section_four.php
@@ -0,0 +1,72 @@
+<section>
+
+
+  <div class="container news-cour">
+  <div class="row">
+    <div class="col-sm side-news">
+
+    <ul class="list-group list-group-flush news-cat">
+    <li class="list-group-item cat-main-news">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Morbi leo risus</li>
+    <li class="list-group-item">Porta ac consectetur ac</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
+    </div>
+
+
+
+    <div class="col-sm">
+    <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-end">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+    </ul>
+    </nav>
+   
+
+    <div class="container cour-news">
+  <div class="row ">
+    <div class="col-sm news-card">
+    <p class = "news-serv news-head">Innovation<br>
+    <span>
+    Our Services<span><p>
+
+    <div class="card card-news-one" style="width: 18rem;">
+  <img src="./assets/images/20-mg-label-blister-pack-208512.jpg" class="card-img-top" alt="">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="col-sm news-card">
+    <div class="card card-news-two" style="width: 18rem;">
+  <img src="./assets/images/person-in-white-hand-gloves-writing-on-white-paper-207601.jpg" class="card-img-top" alt="">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="col-sm news-card">
+    <div class="card card-news-three " style="width: 18rem;">
+  <img src="./assets/images/person-getting-his-blood-check-1350560.jpg" class="card-img-top" alt="">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+    
+
+  </div>
+</div>
+
+</section>
