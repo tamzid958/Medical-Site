194f8dcfce3b4c3fa1a1326fa9a286b24d3e69a0
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-23 00:12:52 +0600

    messenger chat

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,3 @@
-
 <!--JS File always in footer to load faster-->
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
