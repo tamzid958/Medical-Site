1d2908a9153d0bc4923fd1b11aadb5a45fcb3660
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-23 20:39:21 +0600

    messenger add

--- timetable.php
+++ timetable.php
@@ -9,8 +9,31 @@ include('page_sections/timetable_sections/mobile_section.php');
 include('page_sections/timetable_sections/pc_tab_section.php');
 ?>
 
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v7.0'
+          });
+        };
 
-<body>
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="107467831029040"
+  theme_color="#00c4cc">
+      </div>
+
     
 </body>
 
