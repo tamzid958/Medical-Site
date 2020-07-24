ef174d15e9cfb36c5665f36b7905baea2c6aaf6e
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-24 15:33:16 +0600

    ds

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,7 @@
     <meta name="theme-color" content="#00c4cc">
     <!-- favicon-->
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-    <link rel="shortcut icon" href="http://<?php echo $_SERVER['HTTP_HOST']?>/assets/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/x-icon">
     <link rel="icon" href="./assets/images/favicon.ico" type="image/x-icon">
 
     <!-- stylesheet connected-->
