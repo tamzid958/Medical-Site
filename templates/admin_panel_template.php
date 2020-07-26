5ca0545c9194bdc301b3ce30c712ef70186fec31
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-27 02:57:35 +0600

    done

--- templates/admin_panel_template.php
+++ templates/admin_panel_template.php
@@ -37,7 +37,7 @@
   <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand desktop_logo" href="/index.php"><img src="/assets/images/white_logo.png"></a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler position-absolute d-md-none collapsed admin-ham-btn" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
