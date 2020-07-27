2aaf019912ade1ba02d09a8fff9d814238a80677
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-27 11:05:56 +0600

    map added

--- page_sections/about_sections/section_four.php
+++ page_sections/about_sections/section_four.php
@@ -73,5 +73,5 @@
   </div>
 </div>
 
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233668.38703692693!2d90.27923991057244!3d23.780573258035957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1595825822679!5m2!1sen!2sbd" width= "100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 </section>
