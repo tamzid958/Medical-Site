d74031eedd9b9d12b9a3e5711eb11c688ffd5cdf
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-09-05 01:43:50 +0600

    fixed

--- controller/sessionDie.php
+++ controller/sessionDie.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+ob_start();
 $_SESSION["logged_in"] = false;
 session_destroy();
 header("Location: ../login.php");
--- page_sections/admin_panel_sections/admin_settings_seciton.php
+++ page_sections/admin_panel_sections/admin_settings_seciton.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+ob_start();
 require_once '../../controller/Controller.php';
 ?>
 <div class="container">
