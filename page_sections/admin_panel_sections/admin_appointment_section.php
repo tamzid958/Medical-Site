c5692eb8080dc3ebe1813a72407c1819b9f007e2
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-09-05 15:42:17 +0600

    past date block

--- page_sections/admin_panel_sections/admin_appointment_section.php
+++ page_sections/admin_panel_sections/admin_appointment_section.php
@@ -327,7 +327,18 @@ $appointments = getAllAppointments();
             }
         }
 
+        var dtToday = new Date();
 
+        var month = dtToday.getMonth() + 1;
+        var day = dtToday.getDate();
+        var year = dtToday.getFullYear();
+        if (month < 10)
+            month = '0' + month.toString();
+        if (day < 10)
+            day = '0' + day.toString();
+
+        var maxDate = year + '-' + month + '-' + day;
+        $('#date_patient').attr('min', maxDate);
 
 
 
--- page_sections/appointment_sections/section_one.php
+++ page_sections/appointment_sections/section_one.php
@@ -130,6 +130,19 @@ $appointments = getAllAppointments();
   <script>
     $('document').ready(function() {
 
+      var dtToday = new Date();
+
+      var month = dtToday.getMonth() + 1;
+      var day = dtToday.getDate();
+      var year = dtToday.getFullYear();
+      if (month < 10)
+        month = '0' + month.toString();
+      if (day < 10)
+        day = '0' + day.toString();
+
+      var maxDate = year + '-' + month + '-' + day;
+      $('#date_patient').attr('min', maxDate);
+
       $('#time_patient').on('change', function() {
 
 
